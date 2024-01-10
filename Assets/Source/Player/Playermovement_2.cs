89114400ec5b7b9e427fa16e722187b7e52a6c9b
TheFalsMaps <Spirin.Andrew1@mail.ru>
2024-01-11 00:10:28 +0300

    2 variants of movement
    2 variants of movement

--- Assets/Source/Player/Playermovement_2.cs
+++ Assets/Source/Player/Playermovement_2.cs
@@ -5,30 +5,10 @@ namespace Source.Player
     public class Playermovement_2 : MonoBehaviour
     {
         public float moveSpeed = 5f;
-        public float speed;
-        public Camera camera;
-
         void Update()
         {
-            if (Input.GetKey(KeyCode.Mouse1))
-            {
-                Look(); 
-            }
+            
             Move();
-        }
-
-        void Look()
-        {
-            Plane playerPlane = new Plane(Vector3.up, transform.position);
-            Ray ray = camera.ScreenPointToRay(Input.mousePosition);
-            float hitdist;
-
-            if (playerPlane.Raycast(ray, out hitdist))
-            {
-                Vector3 targetPoint = ray.GetPoint(hitdist);
-                Quaternion targetRotation = Quaternion.LookRotation(targetPoint - transform.position);
-                transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, speed * Time.deltaTime);
-            }
         }
         void Move()
         {
