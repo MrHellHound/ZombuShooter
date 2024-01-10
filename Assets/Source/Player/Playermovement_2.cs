a70b5ff72f2f2a1998896b6378dbad72c20b8bf0
TheFalsMaps <Spirin.Andrew1@mail.ru>
2024-01-11 00:17:00 +0300

    Final movement fix
    Final movement fix

--- Assets/Source/Player/Playermovement_2.cs
+++ Assets/Source/Player/Playermovement_2.cs
@@ -5,9 +5,9 @@ namespace Source.Player
     public class Playermovement_2 : MonoBehaviour
     {
         public float moveSpeed = 5f;
+        public float sightSpeed = 2f;
         void Update()
         {
-            
             Move();
         }
         void Move()
@@ -15,9 +15,17 @@ namespace Source.Player
             
             float horizontalInput = Input.GetAxis("Horizontal");
             float verticalInput = Input.GetAxis("Vertical");
-
-            Vector3 movement = new Vector3(horizontalInput, 0f, verticalInput) * moveSpeed * Time.deltaTime;
-            transform.Translate(movement);
+            
+            if (Input.GetKey(KeyCode.Mouse1))
+            {
+                Vector3 movement = new Vector3(horizontalInput, 0f, verticalInput) * sightSpeed * Time.deltaTime;
+                transform.Translate(movement);
+            }
+            else
+            {
+                Vector3 movement = new Vector3(horizontalInput, 0f, verticalInput) * moveSpeed * Time.deltaTime;
+                transform.Translate(movement);
+            }
         }
     }
 }
