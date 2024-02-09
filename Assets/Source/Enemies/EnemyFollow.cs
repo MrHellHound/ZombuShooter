608dbf65095212a99e3570b26842abbffc84dd93
HellHound <Spirin.Andrew1@mail.ru>
2024-02-09 20:19:44 +0300

    AI behavior WIP
    AI behavior WIP

--- Assets/Source/Enemies/EnemyFollow.cs
+++ Assets/Source/Enemies/EnemyFollow.cs
@@ -10,31 +10,34 @@ namespace Source.Enemies
         
         private Vector3 _initialPosition;
         private bool _isAttacking;
+        private bool _bulletInRange;
 
         private void Start()
         {
             _isAttacking = false;
+            _bulletInRange = false;
         }
 
         private void Update()
         {
             _initialPosition = transform.position;
+
+            if (_bulletInRange && !_isAttacking)
+            {
+                StartCoroutine(Offensive());
+            }
         }
 
         private void OnTriggerStay(Collider other)
         {
             if (other.CompareTag("Player") && !_isAttacking)
             {
-                Vector3 direction = player.position - transform.position;
-
-                direction.Normalize();
-                
-                transform.position = Vector3.MoveTowards(transform.position, player.position, moveSpeed * Time.deltaTime);
+                FollowPlayer();
             }
 
             if (other.CompareTag("Bullet") && !_isAttacking)
             {
-                StartCoroutine(Offensive());
+                _bulletInRange = true;
             }
         }
         private void OnTriggerExit(Collider other)
@@ -60,23 +63,35 @@ namespace Source.Enemies
                 StopCoroutine(Attack());
             }
         }
-
-        private IEnumerator Offensive()
+        
+        private void FollowPlayer()
         {
             Vector3 direction = player.position - transform.position;
 
             direction.Normalize();
                 
             transform.position = Vector3.MoveTowards(transform.position, player.position, moveSpeed * Time.deltaTime);
+        }
+        
+        private IEnumerator Offensive()
+        {
+            FollowPlayer();
 
             yield return new WaitForSeconds(3f);
+
+            transform.position = _initialPosition;
+
+            _bulletInRange = false;
         }
         
         private IEnumerator Attack()
         {
             _isAttacking = true;
+            
             transform.position = _initialPosition; 
+            
             yield return new WaitForSeconds(1f);
+            
             _isAttacking = false;
         }
     }
