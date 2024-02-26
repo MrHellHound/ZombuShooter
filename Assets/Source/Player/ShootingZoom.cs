235f93c7d455ec0221059984b929e668a895b8be
HellHound <Spirin.Andrew1@mail.ru>
2024-02-26 06:14:17 +0300

    TPS Remake
    TPS Remake

--- Assets/Source/Player/ShootingZoom.cs
+++ Assets/Source/Player/ShootingZoom.cs
@@ -1,11 +1,12 @@
+using Invector.vCharacterController;
 using UnityEngine;
 
 namespace Source.Player
 {
     public class ShootingZoom : MonoBehaviour
     {
-        //[SerializeField] public vThirdPersonController vThirdPersonController;
-        //[SerializeField] public vThirdPersonCamera vThirdPersonCamera;
+        [SerializeField] public vThirdPersonController vThirdPersonController;
+        [SerializeField] public vThirdPersonCamera vThirdPersonCamera;
         [SerializeField] private float transitionSpeed = 2f;
         
         private float _targetRightOffset;
@@ -14,22 +15,22 @@ namespace Source.Player
 
         private void Update()
         {
-            //if (vThirdPersonController.isStrafing)
-         //   {
-           //     _targetRightOffset = 0.3f;
-           //     _targetDefaultDistance = 1.7f;
-            //    _targetHeight = 1.65f;
-            //}
-           // else
+            if (vThirdPersonController.isStrafing)
+            {
+                _targetRightOffset = 0.3f;
+                _targetDefaultDistance = 1.7f;
+                _targetHeight = 1.65f;
+            }
+            else
             {
                 _targetRightOffset = 0.15f;
                 _targetDefaultDistance = 2f;
                 _targetHeight = 1.4f;
             }
             
-            //vThirdPersonCamera.rightOffset = Mathf.Lerp(vThirdPersonCamera.rightOffset, _targetRightOffset, Time.deltaTime * transitionSpeed);
-            //vThirdPersonCamera.defaultDistance = Mathf.Lerp(vThirdPersonCamera.defaultDistance, _targetDefaultDistance, Time.deltaTime * transitionSpeed);
-            //vThirdPersonCamera.height = Mathf.Lerp(vThirdPersonCamera.height, _targetHeight, Time.deltaTime * transitionSpeed);
+            vThirdPersonCamera.rightOffset = Mathf.Lerp(vThirdPersonCamera.rightOffset, _targetRightOffset, Time.deltaTime * transitionSpeed);
+            vThirdPersonCamera.defaultDistance = Mathf.Lerp(vThirdPersonCamera.defaultDistance, _targetDefaultDistance, Time.deltaTime * transitionSpeed);
+            vThirdPersonCamera.height = Mathf.Lerp(vThirdPersonCamera.height, _targetHeight, Time.deltaTime * transitionSpeed);
         }
     }
 }
